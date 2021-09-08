3f1c8c7e3e50f2eeaa82a562dacea406180f69f0
Daniel Pires <dfpires@gmail.com>
2021-09-08 19:44:24 -0300

    08-09

---
+++ funcoes.js
@@ -0,0 +1,18 @@
+function principal(){
+
+    f1()
+    f2()
+    f3()
+}
+
+function f1(){
+    alert(`Chamada de F1`)
+}
+
+function f2(){
+    alert(`Chamada de F2`)
+}
+
+function f3(){
+    alert(`Chamada de F3`)
+}
