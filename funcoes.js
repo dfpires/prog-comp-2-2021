5bbf33e85f8ffcc99e9d5e1e1861620cae444709
Daniel Pires <dfpires@gmail.com>
2021-09-08 20:36:04 -0300

    08-09

--- funcoes.js
+++ funcoes.js
@@ -7,10 +7,9 @@ function principal(){
         opcao = Number(prompt(`Digite 1. F1 2. F2 3. F3 4. Sair`))
         switch(opcao){
             case 1: idade = Number(prompt(`Informe sua idade`))
-                    f1(idade)
+                    idade = f1(idade)
                     break
-            case 2: idade = Number(prompt(`Informe sua idade`))
-                    peso = Number(prompt(`Informe seu peso`))
+            case 2: peso = Number(prompt(`Informe seu peso`))
                     f2(idade, peso)
                     break
             case 3:  idade = Number(prompt(`Informe sua idade`))
@@ -30,12 +29,14 @@ function principal(){
 function f1(idade){
     // let idade = 10
      alert(`Valor de idade ${idade}`)
+     idade = idade + 10
+     return idade // retorna o resultado da função
 }
 // função que recebe parâmetro
 function f2(idade, peso){
      alert(`Valor de idade ${idade} e do peso ${peso}`)
 }
 // função que recebe parâmetro
-function f3(idade){
-    alert(`Valor de idade ${idade}`)
+function f3(idade, peso, altura){
+    alert(`Valor de idade ${idade}, valor do peso ${peso} e valor da altura ${altura}`)
 }
